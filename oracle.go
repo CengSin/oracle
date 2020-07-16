47f36f8eebb2ba15eafef959780267cf5c0529d7
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-16 23:25:50 +0800

    set to use create index after create table because the syntax is compatible

--- oracle.go
+++ oracle.go
@@ -35,29 +35,29 @@ func New(config Config) gorm.Dialector {
 	return &Dialector{Config: &config}
 }
 
-func (dialector Dialector) Name() string {
+func (d Dialector) Name() string {
 	return "oracle"
 }
 
-func (dialector Dialector) Initialize(db *gorm.DB) (err error) {
+func (d Dialector) Initialize(db *gorm.DB) (err error) {
 	// register callbacks
 	callbacks.RegisterDefaultCallbacks(db, &callbacks.Config{})
 
-	dialector.DriverName = "godror"
+	d.DriverName = "godror"
 
-	if dialector.Conn != nil {
-		db.ConnPool = dialector.Conn
+	if d.Conn != nil {
+		db.ConnPool = d.Conn
 	} else {
-		db.ConnPool, err = sql.Open(dialector.DriverName, dialector.DSN)
+		db.ConnPool, err = sql.Open(d.DriverName, d.DSN)
 	}
 
-	for k, v := range dialector.ClauseBuilders() {
+	for k, v := range d.ClauseBuilders() {
 		db.ClauseBuilders[k] = v
 	}
 	return
 }
 
-func (dialector Dialector) ClauseBuilders() map[string]clause.ClauseBuilder {
+func (d Dialector) ClauseBuilders() map[string]clause.ClauseBuilder {
 	return map[string]clause.ClauseBuilder{
 		"LIMIT": func(c clause.Clause, builder clause.Builder) {
 			if limit, ok := c.Expression.(clause.Limit); ok {
@@ -92,27 +92,28 @@ func (dialector Dialector) ClauseBuilders() map[string]clause.ClauseBuilder {
 	}
 }
 
-func (dialector Dialector) DefaultValueOf(*schema.Field) clause.Expression {
+func (d Dialector) DefaultValueOf(*schema.Field) clause.Expression {
 	return clause.Expr{SQL: "VALUES (DEFAULT)"}
 }
 
-func (dialector Dialector) Migrator(db *gorm.DB) gorm.Migrator {
+func (d Dialector) Migrator(db *gorm.DB) gorm.Migrator {
 	return Migrator{
 		Migrator: migrator.Migrator{
 			Config: migrator.Config{
-				DB:        db,
-				Dialector: dialector,
+				DB:                          db,
+				Dialector:                   d,
+				CreateIndexAfterCreateTable: true,
 			},
 		},
 	}
 }
 
-func (dialector Dialector) BindVarTo(writer clause.Writer, stmt *gorm.Statement, v interface{}) {
+func (d Dialector) BindVarTo(writer clause.Writer, stmt *gorm.Statement, v interface{}) {
 	writer.WriteString(":")
 	writer.WriteString(strconv.Itoa(len(stmt.Vars)))
 }
 
-func (dialector Dialector) QuoteTo(writer clause.Writer, str string) {
+func (d Dialector) QuoteTo(writer clause.Writer, str string) {
 	if ReservedWords.Contains(str) {
 		writer.WriteByte('"')
 		defer writer.WriteByte('"')
@@ -122,7 +123,7 @@ func (dialector Dialector) QuoteTo(writer clause.Writer, str string) {
 
 var numericPlaceholder = regexp.MustCompile(`:(\d+)`)
 
-func (dialector Dialector) Explain(sql string, vars ...interface{}) string {
+func (d Dialector) Explain(sql string, vars ...interface{}) string {
 	for idx, v := range vars {
 		if b, ok := v.(bool); ok {
 			if b {
@@ -136,7 +137,7 @@ func (dialector Dialector) Explain(sql string, vars ...interface{}) string {
 	return logger.ExplainSQL(sql, numericPlaceholder, `'`, vars...)
 }
 
-func (dialector Dialector) DataTypeOf(field *schema.Field) string {
+func (d Dialector) DataTypeOf(field *schema.Field) string {
 	if _, found := field.TagSettings["RESTRICT"]; found {
 		delete(field.TagSettings, "RESTRICT")
 	}
@@ -168,7 +169,7 @@ func (dialector Dialector) DataTypeOf(field *schema.Field) string {
 		return sqlType
 	case schema.String:
 		size := field.Size
-		defaultSize := dialector.DefaultStringSize
+		defaultSize := d.DefaultStringSize
 
 		if size == 0 {
 			if defaultSize > 0 {
@@ -217,12 +218,12 @@ func (dialector Dialector) DataTypeOf(field *schema.Field) string {
 	return fmt.Sprintf("%v %v", sqlType, additionalType)
 }
 
-func (dialectopr Dialector) SavePoint(tx *gorm.DB, name string) error {
+func (d Dialector) SavePoint(tx *gorm.DB, name string) error {
 	tx.Exec("SAVEPOINT " + name)
 	return nil
 }
 
-func (dialectopr Dialector) RollbackTo(tx *gorm.DB, name string) error {
+func (d Dialector) RollbackTo(tx *gorm.DB, name string) error {
 	tx.Exec("ROLLBACK TO SAVEPOINT " + name)
 	return nil
 }
