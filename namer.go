d43380d395ca6df9c81ce678eff8418042e91ae3
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2020-07-20 21:49:27 +0800

    partially revert #1eda382f775874cbe4b453a704a26858ec48a90b

--- namer.go
+++ namer.go
@@ -24,3 +24,15 @@ func (n Namer) TableName(table string) (name string) {
 func (n Namer) JoinTableName(table string) (name string) {
 	return ConvertNonReservedWordToCap(n.NamingStrategy.JoinTableName(table))
 }
+
+func (n Namer) RelationshipFKName(relationship schema.Relationship) (name string) {
+	return ConvertNonReservedWordToCap(n.NamingStrategy.RelationshipFKName(relationship))
+}
+
+func (n Namer) CheckerName(table, column string) (name string) {
+	return ConvertNonReservedWordToCap(n.NamingStrategy.CheckerName(table, column))
+}
+
+func (n Namer) IndexName(table, column string) (name string) {
+	return ConvertNonReservedWordToCap(n.NamingStrategy.IndexName(table, column))
+}
