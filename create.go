35481dc4bdbc3a2c1252b8d4336f3a2e0bb20e77
daviyang35 <daviyang35@gmail.com>
2021-01-28 10:06:43 +0800

    Support insert use the username
    支持插入时使用 "用户名.表名" 格式
    insert into user.table () values ()

--- create.go
+++ create.go
@@ -71,7 +71,7 @@ func Create(db *gorm.DB) {
 
 			stmt.Build("MERGE", "WHEN MATCHED", "WHEN NOT MATCHED")
 		} else {
-			stmt.AddClauseIfNotExists(clause.Insert{Table: clause.Table{Name: stmt.Table}})
+			stmt.AddClauseIfNotExists(clause.Insert{Table: clause.Table{Name: stmt.TableExpr.SQL}})
 			stmt.AddClause(clause.Values{Columns: values.Columns, Values: [][]interface{}{values.Values[0]}})
 			if hasDefaultValues {
 				stmt.AddClauseIfNotExists(clause.Returning{
